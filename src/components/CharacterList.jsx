c6e0a277bbafc388ce4fbb85727c80827586d697
ARS <xxx@gmail.com>
2023-07-21 10:11:54 -0600

    Agregar CSS

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -3,25 +3,37 @@ import Character from "./Character";
 
 function CharacterList() {
   const [character, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const response = await fetch("https://rickandmortyapi.com/api/character");
       const data = await response.json();
       //console.log(data); El arreglo por medio de console log nos muestra un objeto con dos propiedades info y results, en la linea siguiente solo le pedieremos results
+      setLoading(false);
       setCharacters(data.results);
     }
 
     fetchData();
   }, []);
 
+
   return (
-    <div>
-      {character.map((character) => {
-        return (
-          <Character key={character.id} character={character}/>
-        );
-      })}
+    <div className="container">
+      {
+        loading ? (<h1>Cargadon datos</h1>) : (
+          <div className="row">
+            {character.map((character) => {
+              return (
+                <div className="col-md-4" key={character.id} >
+                  <Character character={character} />
+                </div>
+              );
+            })}
+          </div>
+        )
+      }
+
     </div>
   );
 }
